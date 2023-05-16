65101ac43120b1725ce39ffc4eae485ef5364673
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-05-16 23:45:48 +0530

    ServicesPage

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-
-
 function menu(){
     const nav =  document.getElementsByClassName("menu")[0];
     const icon = document.getElementById('icon');
@@ -12,3 +10,31 @@ function menu(){
          icon.setAttribute("class","fa-solid fa-bars icon");
     }
  }
+
+ function smoothScrollToId(id) {
+     const element = document.getElementById(id);
+     if (element) {
+       const elementPosition = element.getBoundingClientRect().top;
+       const startPosition = window.pageYOffset;
+       const distance = elementPosition - startPosition;
+       const duration = 500; // set the duration of the scroll animation in milliseconds
+       let startTime = null;
+   
+       function scrollAnimation(currentTime) {
+         if (startTime === null) startTime = currentTime;
+         const timeElapsed = currentTime - startTime;
+         const scroll = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+         window.scrollTo(0, scroll);
+         if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+       }
+   
+       function easeInOutQuad(t, b, c, d) {
+         t /= d/2;
+         if (t < 1) return c/2*t*t + b;
+         t--;
+         return -c/2 * (t*(t-2) - 1) + b;
+       }
+   
+       requestAnimationFrame(scrollAnimation);
+     }
+   }
